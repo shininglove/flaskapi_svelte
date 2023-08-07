18954f906d6518a95e4bb4d60cd7689ed222cffe
PyAss <pyassistant00@gmail.com>
2023-08-06 22:38:12 -0400

    initial queryless + tailwind / skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
+    import "@skeletonlabs/skeleton/styles/skeleton.css";
+    import "../app.postcss";
+    import { browser } from "$app/environment";
+    import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                enabled: browser,
+            },
+        },
+    });
+</script>
+
+<QueryClientProvider client={queryClient}>
+    <slot />
+</QueryClientProvider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { z } from "zod";
+    import { createZodFetcher } from "zod-fetch";
+
+    const host: string = import.meta.env.VITE_HOST_URL;
+
+    const fetchWithZod = createZodFetcher();
+
+    export let info: Promise<string> | null;
+
+    onMount(async () => {
+        info = dataFetcher();
+    });
+
+    async function dataFetcher() {
+        const res = await fetchWithZod(
+            z.object({ hello: z.string() }),
+            `${host}/data`
+        );
+        console.log(res);
+        return res.hello;
+    }
+
+</script>
+
+<section>
+    <div>About</div>
+    <div>Element goes zzzooommm!</div>
+    {#await info}
+        <p>loading...</p>
+    {:then myInfo}
+        {#if myInfo}
+            <p>The info is: {myInfo}</p>
+        {/if}
+    {:catch error}
+        <p style="color: red;">{error.message}</p>
+    {/await}
+</section>
